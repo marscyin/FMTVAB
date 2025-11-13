e1d96063f35e70d4f22184d1f0a3af013ac72dad
marscyin <30969023+marscyin@users.noreply.github.com>
2025-11-13 20:20:47 +0800

    Update DIY.sh

--- DIY/DIY.sh
+++ DIY/DIY.sh
@@ -7,5 +7,8 @@ find . -name "strings.xml" -exec sed -i "s/<string name=\"app_name\">.*<\/string
 
 # ② AndroidManifest.xml 桌面 banner 文字（TV 启动器下方标题）
 sed -i "s/android:label=\"@string\/app_name\"/android:label=\"$APP_NAME\"/" app/src/main/AndroidManifest.xml
+sed -i '/<application/ s/>/ tools:replace="android:label">/' app/src/main/AndroidManifest.xml
+sed -i '/<application/ s/>/ tools:replace="android:label">/' app/src/leanback/AndroidManifest.xml
+
 
 echo "✅ 应用名已改为：$APP_NAME"
